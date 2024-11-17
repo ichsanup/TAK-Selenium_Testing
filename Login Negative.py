6c7e697b99a6350ce1d763ef0e69a74979b97afa
ichsanup <ichsanup@gmail.com>
2024-11-17 09:37:33 +0700

    negative case

--- Login Negative.py
+++ Login Negative.py
@@ -7,12 +7,23 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Firefox()
 driver.get("https://demowebshop.tricentis.com/")
 
+# Empty Email
+LoginBtn = driver.find_element(By.CLASS_NAME, "ico-login")
+LoginBtn.click()
+Email = driver.find_element(By.ID, Elemen.email)
+Email.send_keys('')
+Password = driver.find_element(By.ID, Elemen.password)
+Password.send_keys('Password123')
+BtnLogin = driver.find_element(By.XPATH, Elemen.login_btn)
+BtnLogin.click()
+
+# Empty Password
 LoginBtn = driver.find_element(By.CLASS_NAME, "ico-login")
 LoginBtn.click()
 Email = driver.find_element(By.ID, Elemen.email)
 Email.send_keys('ichsanup@gmail.com')
 Password = driver.find_element(By.ID, Elemen.password)
-Password.send_keys('Password123')
+Password.send_keys('')
 BtnLogin = driver.find_element(By.XPATH, Elemen.login_btn)
 BtnLogin.click()
 
@@ -24,10 +35,11 @@ try:
 except Exception as e:
     print("Elemen tidak ditemukan:", e)
 
+# Empty Input Data Buyer
 Recipent_Name = driver.find_element(By.ID, Elemen.recipient_name)
-Recipent_Name.send_keys("Ichsan")
+Recipent_Name.send_keys("")
 Recipent_Email = driver.find_element(By.ID, Elemen.recipient_email)
-Recipent_Email.send_keys("ichsanup@gmail.com")
+Recipent_Email.send_keys("")
 Btn_Cart = driver.find_element(By.ID, Elemen.btn_cart)
 Btn_Cart.click()
 
@@ -40,12 +52,7 @@ except Exception as e:
     print("Expected Result: Elemen tidak dapat diklik.", e)
 
 ChecklistBtn = driver.find_element(By.ID, Elemen.checklist_btn)
-# ChecklistBtn.click()
-try:
-    ActionChains(driver).move_to_element(ChecklistBtn).click().perform()
-    # print("Expected Result: Elemen dapat diklik.")
-except Exception as e:
-    print("Expected Result: Elemen tidak dapat diklik.", e)
+ChecklistBtn.click()
 CheckoutBtn = driver.find_element(By.ID, Elemen.checkout_btn)
 CheckoutBtn.click()
 
